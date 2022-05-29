b44d20b8e33007e61c825f79b404ad769a098ebc
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-05-29 13:58:10 +0300

    created image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    post_time = models.DateTimeField(auto_now_add=True)
+    
