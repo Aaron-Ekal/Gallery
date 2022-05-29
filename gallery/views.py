d6b55e5b919ea30855b73038d5c5101c44e4297d
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-05-29 16:11:16 +0300

    adds image display feature

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Image
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, Http404
 
 # Create your views here.
-def home(request):
-    return render(request, 'index.html')
+def gallery(request):
+    categories = Image.objects.distinct().values_list('category__name', flat=True)
+    locations = Image.objects.distinct().values_list('location__name', flat=True)
+    try:
+        images = Image.objects.all()
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request, 'gallery.html', {'image': images, 'categories': categories, 'locations': locations})
