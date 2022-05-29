7b8e85ed3ac5f4d35354bb1d16b1270f53dfe31b
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-05-29 17:43:07 +0300

    configures bootstrap to work

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', views.gallery, name='gallery')
+    path('', views.gallery, name='gallery'),
+    path('search/', views.search_images, name="search_images"),
+    path('category/<category>', views.view_category, name='view_category'),
+    path('location/<location>', views.view_location, name='view_location')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gallery/views.py
+++ gallery/views.py
@@ -12,3 +12,29 @@ def gallery(request):
     except ObjectDoesNotExist:
         raise Http404()
     return render(request, 'gallery.html', {'image': images, 'categories': categories, 'locations': locations})
+
+
+def search_images(request):
+    if 'image' in request.GET and request.GET['image']:
+        search_term = request.GET.get('image')
+        searched_images = Image.search_by_name(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'message': message, 'images': searched_images})
+    else:
+        message = "You haven't searched for any search term"
+        return render(request, 'search.html', {'message': message})
+    
+    
+def view_category(request, category):
+    categories = Image.objects.distinct().values_list('category__name', flat=True)
+    locations = Image.objects.distinct().values_list('location__name', flat=True)
+    image = Image.objects.filter(category__name=category)
+    return render(request, 'category.html', {"image": image, 'categories': categories, 'locations': locations})
+
+
+def view_location(request, location):
+    locations = Image.objects.distinct().values_list('location__name', flat=True)
+    categories = Image.objects.distinct().values_list('category__name', flat=True)
+    image = Image.objects.filter(location__name=location)
+    return render(request, 'category.html', {"image": image, "locations": locations, 'categories': categories})
